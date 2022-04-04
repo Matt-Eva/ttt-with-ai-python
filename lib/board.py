7c25c54738d46b19b2c78f5f8bb7890cdb63096b
Matt E <matteweva@gmail.com>
2022-04-04 10:58:14 -0700

    created board functions

--- lib/board.py
+++ lib/board.py
@@ -1,2 +1,26 @@
 class Board:
   pass
+
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.cells= ["1", "", "", "", "", "", "", "", ""]
+
+  def display(self):
+    print(self.cells)
+  
+  def position(self, string):
+      index = int(string) -1
+      return self.cells[index]
+  
+  def update(self, string, player):
+    index = int(string) - 1
+    self.cells[index] = player.token()
+
+board = Board()
+board.display()
+
+board.position('1')
+
+board.update('1')
